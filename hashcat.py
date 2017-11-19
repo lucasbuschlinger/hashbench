170f8e9d9adfbc75739de83530b3bd582e7a78a9
Lucas Buschlinger <misc@lucas-buschlinger.de>
2017-11-19 18:00:12 +0100

    Removed time watchdog from hashcat as it has that built in

--- hashcat.py
+++ hashcat.py
@@ -1,7 +1,4 @@
 import subprocess
-import time
-import threading
-import queue
 from utils import *
 
 # Function to capture and filter hashcats output
@@ -52,37 +49,16 @@ def hashcat_wordlist(hash, hash_file, wordlist, rules, max_exec_time):
 
     if rules == None:
         # Spawn subprocess running an instance of hashcat without rules
-        process = subprocess.Popen(["./hashcat/hashcat", "-a0", "-m", "{}".format(hash), hash_file, wordlist, "--status", "--status-timer", "1"],  universal_newlines=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        process = subprocess.Popen(["./hashcat/hashcat", "-a0", "-m", "{}".format(hash), hash_file, wordlist, "--status", "--status-timer", "1", "-w", "3", "-O""--runtime={}".format(max_exec_time), "-o", "/dev/null"],  universal_newlines=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     else:
         # Spawn subprocess running an instance of hashcat with rules
-        process = subprocess.Popen(["./hashcat/hashcat", "-a0", "-m", "{}".format(hash), hash_file, wordlist, "--status", "--status-timer", "1", "-r", rules],  universal_newlines=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        process = subprocess.Popen(["./hashcat/hashcat", "-a0", "-m", "{}".format(hash), hash_file, wordlist, "--status", "--status-timer", "1", "-r", "-w", "3", "-O",rules, "--runtime={}".format(max_exec_time), "-o", "/dev/null"],  universal_newlines=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
     # List to store number of cracked hashes and speeds
     speeds = [0]
 
-    # Queue to communicate with the timeout thread, enables us to end it, if the process ends prior to timeout
-    com_queue = queue.Queue(maxsize=1)
-
-    # This thread captures the output from hashcat
-    thread_output = threading.Thread(target=hashcat_out, args=(process, speeds))
-    thread_output.start()
-    # This thread keeps a look on the time(out)
-    thread_timeout = threading.Thread(target=time_watcher, args=(max_exec_time, com_queue))
-    thread_timeout.start()
-
-    # While both treads are running OR, if no maximum execution time was specified, while hashcat is running we do absolutely nothing (wasting cycles... IS THERE A BETTER WAY?)
-    while(threading.active_count() == 3 or (no_time and (threading.active_count() == 2))):
-        pass
-
-    # Terminating hashcat if timeout is reached
-    if(thread_output.is_alive()):
-        process.terminate()
-    thread_output.join()
-
-    # Terminating timeout thread if hashcat is done earlier
-    if(thread_timeout.is_alive()):
-        com_queue.put("Exit")
-    thread_timeout.join()
+    # Calling the output collector
+    hashcat_out(process, speeds)
 
     # Putting number of cracked hashes into returning value
     return_values = [speeds.pop(0)]
@@ -113,29 +89,8 @@ def hashcat_bruteforce(hash, min, max, hash_file, max_exec_time):
     # List to store number of cracked hashes and speeds
     speeds = [0]
 
-    # Queue to communicate with the timeout thread, enables us to end it, if the process ends prior to timeout
-    com_queue = queue.Queue(maxsize=1)
-
-    # This thread captures the output from hashcat
-    thread_output = threading.Thread(target=hashcat_out, args=(process, speeds))
-    thread_output.start()
-    # This thread keeps a look on the time(out)
-    thread_timeout = threading.Thread(target=time_watcher, args=(max_exec_time, com_queue))
-    thread_timeout.start()
-
-    # While both treads are running OR, if no maximum execution time was specified, while hashcat is running we do absolutely nothing (wasting cycles... IS THERE A BETTER WAY?)
-    while(threading.active_count() == 3 or (no_time and (threading.active_count() == 2))):
-        pass
-
-    # Terminating hashcat if timeout is reached
-    if(thread_output.is_alive()):
-        process.terminate()
-    thread_output.join()
-
-    # Terminating timeout thread if hashcat is done earlier
-    if(thread_timeout.is_alive()):
-        com_queue.put("Exit")
-    thread_timeout.join()
+    # Calling the output collector
+    hashcat_out(process, speeds)
 
     # Putting number of cracked hashes into returning value
     return_values = [speeds.pop(0)]
